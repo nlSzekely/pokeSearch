d73755946cbe261e489f79632d22fdeb4ec9b1e4
nlSzekely <32170516+nlSzekely@users.noreply.github.com>
2020-04-13 15:08:18 +0300

    Add files via upload

---
+++ precache-manifest.e333e1521e1522c2b121649da765ee59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f960e6c346aa617e3f50761f9e5415d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4472674c86fec945360d",
+    "url": "./static/css/main.cfa652c0.chunk.css"
+  },
+  {
+    "revision": "2c01a8ea9b161696c40d",
+    "url": "./static/js/2.d3c7f6f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d3c7f6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4472674c86fec945360d",
+    "url": "./static/js/main.c33beaf2.chunk.js"
+  },
+  {
+    "revision": "a1dac3bfd5181488a0b8",
+    "url": "./static/js/runtime-main.f9a4a1c3.js"
+  }
+]);
